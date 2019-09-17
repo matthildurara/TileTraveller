889bbcee07e6bff8830e8bf1e0101ee7508dc145
Matthildur <matthildur@MacBook-Pro-5.local>
2019-09-17 13:58:07 +0000

    First commit

---
+++ tile_traveller.py
@@ -0,0 +1,5 @@
+#start by input from the user that says what direction he wants to move
+# Create a function that makes the user start in the first tile
+# Create a function that tells the user what directions he can move in
+# and tell him when it is not a valid direction and let him choose again.
+# Create a function that tells the user when he reaches the last tile
