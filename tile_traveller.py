bc8b23ad2c5257c94c13afc14a3a41a7fdad9955
Matthildur <matthildur@MacBook-Pro-5.local>
2019-09-17 15:51:52 +0000

    hjk

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,3 +1,9 @@
+#vita staðsstnðinug 
+#vita ver hann ma´fara þaðan
+#og segja þaða
+#taka við innntakinu
+#senda það í fall sem færir hann 
+#vta hvort það er komin sigur
 #start by getting an input from the user that says what direction he wants to move
 # Create a function that makes the user start in the first tile
 # Create a function that tells the user what directions he can move in
@@ -5,37 +11,44 @@
 # Create a function that tells the user when he reaches the last tile
 
 
-def valid_diretions(move):
-tile = 1
-for i in range(1,10):
-    valid_direction = 'nNeEsSwW'
-    if tile = 1:
-        print(valid_direction[0:2])
-    elif tile = 2:
-        print(valid_direction[0:7])
-    elif tile = 3:
-        print(valid_direction[2:6])
-    elif tile = 4:
-        print(valid_direction[0:2])
-    elif tile = 5:
-        print(valid_direction[4:])
-    elif tile = 6:
-        print(valid_direction[2:4]and print(valid_direction[6:]))
-    elif tile = 7:
-        print("Victory!")
-    elif tile = 8:
-        print(valid_direction[0:3] and print(valid_direction[4:6]))
-    elif tile = 9:
-        print(valid_direction[4:])
+NORTH = 'n'
+SOUTH = 's'
+WEST = 'w'
+EAST = 'e'
+
+while position != 3,1
+
+    row,column = 1,1
+    print(valid_direction(a,b))
+    def move_direction():
+        if direction == NORTH:
+            row += 1
+        elif direction == SOUTH:
+            row -= 1
+        elif direction == WEST:
+            column -= 1
+        elif direction == EAST:
+            column += 1
 
 
-def input_direction():
 
-def moving_directions():
+def move_direction(move):
+    move = input("Direction: ")
+
+def valid_direction(a,b)
+    for ch in valid_direction:
+        if ch == NORTH:
+            print("(N)orth")
+        elif ch == SOUTH:
+            print("(S)outh")
+        elif ch == WEST:
+            print("(W)est")
+        elif ch == EAST:
+            print("(E)ast")
+    
+else:
+    print("Victory!")
 
 
-print('Not a valid direction!')
-print('You can travel: ', )
-print('Direction: ')
-
+print("You can travel: ", valid_direction )
 
