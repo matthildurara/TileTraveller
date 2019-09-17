417355eb341c6065b2b8ca0a3abb8a5bfc9d199a
Matthildur <matthildur@MacBook-Pro-5.local>
2019-09-17 14:53:42 +0000

    valid_direction function modified

--- tile_traveller.py
+++ tile_traveller.py
@@ -5,7 +5,29 @@
 # Create a function that tells the user when he reaches the last tile
 
 
-def valid_diretions();
+def valid_diretions(move):
+tile = 1
+for i in range(1,10):
+    valid_direction = 'nNeEsSwW'
+    if tile = 1:
+        print(valid_direction[0:2])
+    elif tile = 2:
+        print(valid_direction[0:7])
+    elif tile = 3:
+        print(valid_direction[2:6])
+    elif tile = 4:
+        print(valid_direction[0:2])
+    elif tile = 5:
+        print(valid_direction[4:])
+    elif tile = 6:
+        print(valid_direction[2:4]and print(valid_direction[6:]))
+    elif tile = 7:
+        print("Victory!")
+    elif tile = 8:
+        print(valid_direction[0:3] and print(valid_direction[4:6]))
+    elif tile = 9:
+        print(valid_direction[4:])
+
 
 def input_direction():
 
@@ -15,5 +37,5 @@ def moving_directions():
 print('Not a valid direction!')
 print('You can travel: ', )
 print('Direction: ')
-print('Victory!')
+
 
