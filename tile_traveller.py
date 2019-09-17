70e92135ad86ecdb6bdfffbf0c2e9b41f8f1b027
Bertha María Óladóttir <berthamola@Berthas-MacBook-Air.local>
2019-09-17 14:22:38 +0000

    created the skeleton

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,5 +1,19 @@
-#start by input from the user that says what direction he wants to move
+#start by getting an input from the user that says what direction he wants to move
 # Create a function that makes the user start in the first tile
 # Create a function that tells the user what directions he can move in
 # and tell him when it is not a valid direction and let him choose again.
 # Create a function that tells the user when he reaches the last tile
+
+
+def valid_diretions();
+
+def input_direction():
+
+def moving_directions():
+
+
+print('Not a valid direction!')
+print('You can travel: ', )
+print('Direction: ')
+print('Victory!')
+
